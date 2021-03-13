e9e338072ea21bd1bcbae3ca6db17ea6fa0267d0
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2021-03-13 11:06:43 +0100

    Spawn flower where player will land

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public GameObject bee;
+    public GameObject flower1Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spawn new flower
+        var planetRadius = GetComponent<SphereCollider>().radius * transform.localScale.x;
+        var beePos = bee.transform.position;
+        var planetPos = transform.position;
+        var flowerPos = (beePos - planetPos).normalized * planetRadius + planetPos;
+        var flower = Instantiate(flower1Prefab);
+        flower.transform.position = flowerPos;
+        flower.transform.up = (flowerPos - planetPos).normalized;
+    }
+}
