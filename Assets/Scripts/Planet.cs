2156857def4c4598cf2ad577f086c73c6ea0feb3
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2021-03-13 13:43:18 +0100

    Make flowers appear with some variance around projected player position

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -6,6 +6,9 @@ public class Planet : MonoBehaviour
 {
     public GameObject bee;
     public GameObject flower1Prefab;
+    public float timeBetweenFlowerSpawns = 0.2f;
+
+    private float timeSinceLastFlowerSpawn = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +19,23 @@ public class Planet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Spawn new flower
+        timeSinceLastFlowerSpawn += Time.deltaTime;
+        if(timeSinceLastFlowerSpawn >= timeBetweenFlowerSpawns)
+        {
+            timeSinceLastFlowerSpawn = 0.0f;
+            SpawnFlower();
+        }
+    }
+
+    private void SpawnFlower()
+    {
         var planetRadius = GetComponent<SphereCollider>().radius * transform.localScale.x;
         var beePos = bee.transform.position;
+        var rangeFactor = 4.0f;
+        var variance = new Vector3(Random.Range(-rangeFactor, rangeFactor), 0, Random.Range(-rangeFactor, rangeFactor));
+        var beePosWithVariance = beePos + variance;
         var planetPos = transform.position;
-        var flowerPos = (beePos - planetPos).normalized * planetRadius + planetPos;
+        var flowerPos = (beePosWithVariance - planetPos).normalized * planetRadius + planetPos;
         var flower = Instantiate(flower1Prefab);
         flower.transform.position = flowerPos;
         flower.transform.up = (flowerPos - planetPos).normalized;
