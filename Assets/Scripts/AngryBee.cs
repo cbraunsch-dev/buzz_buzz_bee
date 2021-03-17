29bfeb3e0297f921e5ea6a687e620d32112c51bd
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2021-03-17 14:02:22 +0100

    Clean up angry bee a bit

--- Assets/Scripts/AngryBee.cs
+++ Assets/Scripts/AngryBee.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// The AngryBee object acts as an anchor that is at the center of the planet. Attached to this anchor is the
+// BeeOnSurface but it is offset by the radius of the planet. Think of the anchor at the center of the planet and an invisible
+// arm with the length of the planet's radius. At the end of that arm is the BeeOnSurface. Swiveling this arm around makes the bee
+// move on the surface of the planet. This is how this works.
 public class AngryBee : MonoBehaviour
 {
     public GameObject targetBee;
