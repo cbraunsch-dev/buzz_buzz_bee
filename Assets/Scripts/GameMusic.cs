37a0991530eb91c7d77b876bf8f0e0c022240a34
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2021-03-20 02:53:56 +0100

    Add game music

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour
+{
+    // The GameMusic is a Singleton that exists throughout the lifetime of the game
+    private static GameMusic instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
